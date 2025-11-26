4056ede3d166addfc10aebfea590636b3a9200bf
thiagoncalvz <thiagoncalvz@gmail.com>
2025-11-26 10:41:25 -0300

    fasfdsa

--- portal-cbmap/resources/views/components/layout.blade.php
+++ portal-cbmap/resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
     <link rel="shortcut icon" href="{{asset('logomarcacbmap.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>{{$title}} - Portal CBMAP</title>
